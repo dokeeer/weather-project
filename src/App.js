4b4b12a290fe2edd5d8d4f239feec7f97521cc93
dokeeer <polit.nikita@gmail.com>
2022-08-25 18:39:38 +0300

    HTML coding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import MainLeft from "./components/MainLeft";
+import MainRight from "./components/MainRight";
 
 function App() {
   return (
-    <div>
-      Test branch
+    <div className='main'>
+        <MainLeft/>
+        <MainRight/>
     </div>
   );
 }
---
+++ src/components/MainRight.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainRight.css'
+
+const MainRight = () => {
+    return (
+        <div className='main--right'>
+            sss
+        </div>
+    );
+};
+
+export default MainRight;
