af6f54a4b869c9853dc5f46679f07f9b0abf8c62
dokeeer <polit.nikita@gmail.com>
2022-08-26 22:03:37 +0300

    Api requesting fixing

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import splitIntoDays from "./splitIntoDays";
+import axios from "axios";
 
 
 
@@ -10,11 +11,8 @@ const getList = (raw) => {
 }
 
 export default async function detData(setter,searchValue) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=6e93b3d15872f914c6929fed9ea71e9a`,{
-        method: 'GET',
-        mode: 'cors'
-    });
-    const data = await response.json();
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=6e93b3d15872f914c6929fed9ea71e9a`);
+    const data = await response.data;
     setter(getList(data))
     console.log('fetched')
 }
