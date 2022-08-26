b5926cf6b83ec2c50e4cf0e611836605e91805c1
dokeeer <polit.nikita@gmail.com>
2022-08-26 12:34:33 +0300

    Added fetching

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import getData from "./data";
 import React from "react";
 
 function App() {
-    getData()
+
+    const [list, setList] = React.useState([])
+
+    React.useEffect(() => {
+        getData(setList)
+
+    }, [])
+    if (list.length !== 0) {
+        const date = new Date((list[0].dt_txt))
+        console.log(date.getDay())
+    }
+
+
   return (
     <div className='main'>
         <MainLeft/>
--- src/data.js
+++ src/data.js
@@ -1,11 +1,15 @@
+
 const url = 'http://api.openweathermap.org/data/2.5/forecast?q=Джанкой&appid=6e93b3d15872f914c6929fed9ea71e9a';
-const options = {
-    method: 'GET',
-};
-export default function getData(){
-    fetch(url)
-        .then(data => data.json())
-        .then(data => {
-            console.log(data);
-        })
+
+
+
+const getList = (raw) => {
+    const { list } = raw
+    return list
+}
+
+export default async function detData(setter) {
+    const response = await fetch(url);
+    const data = await response.json();
+    setter(getList(data))
 }
